6b0b48c7b15def2e3a5f9f91ddbaedf97f814907
xiaoran <ran.xiao@tendcloud.com>
2019-01-06 15:58:59 +0800

    python base

---
+++ base.py
@@ -0,0 +1,29 @@
+# 这是注释
+'''
+多行注释
+'''
+"""
+这也是多行注释
+"""
+
+# 输入输出
+print("请输入任意字符，回车键结束")
+name = input()
+print("name:" + name)
+print("name:", name)
+
+# 数据类型
+num1 = 1
+num2 = 2.2
+num3 = True
+num4 = num1 + num2
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+# True->1   False->2
+print(num1 + num2)
+
+# 空值
+num5 = None
+print(num5)
