3c48ef215b3ce2850aebcfb9c23f097f80fccd9d
xiaoran <xiaoran737@163.com>
2019-01-07 23:38:21 +0800

    列表的简单操作 与或非

--- src/CommonType.py
+++ src/CommonType.py
@@ -12,3 +12,29 @@ print(len(nameByte))
 string = 'Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125)
 # 注意浮点型可以选择小数点
 print(string)
+
+# 列表
+listName = ["123", "hello", 999]
+print(listName)
+print(listName[1:])
+print(listName[0: 1])
+print(listName[-2])
+
+# 与或非运算符
+# 与 and  或  or   非  not
+a = True
+b = False
+c = True
+
+if (a and b):
+    print("yes")
+else:
+    print("no")
+
+if (a or b):
+    print("yes")
+else:
+    print("no")
+
+if (not not c):
+    print("yes")
