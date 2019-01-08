aceb656e5792616dea0610bc6a6e58537b0eb9b6
xiaoran <xiaoran737@163.com>
2019-01-08 22:48:45 +0800

    list的基本操作函数

---
+++ src/List.py
@@ -0,0 +1,21 @@
+# 列表
+list = []
+
+list.append("12")
+list.append("hello")
+list.append("list")
+list.sort()
+list.append(302)
+print("append结果:", list)
+
+list.insert(3, "foreach")
+print("insert结果:", list)
+
+
+
+list.remove(302)
+print("remove结果:", list)
+
+del (list[1])
+print("remove结果:", list)
+
