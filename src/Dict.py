8e92cd453bb392f21a22bce6fbf6a4c9ed413415
xiaoran <xiaoran737@163.com>
2019-01-10 00:22:13 +0800

    字典

---
+++ src/Dict.py
@@ -0,0 +1,13 @@
+# key:value  {}
+dict1 = {"name": "xiaoran", "age": "23", "sex": "male"}
+print(dict1)
+
+# 获取
+sex = dict1['sex']
+print(sex)
+
+print(dict1.keys())
+
+# 修改
+dict1['age'] = 18
+print(dict1)
